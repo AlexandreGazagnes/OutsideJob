b57ce4b007be05e2874479e961994b555bf0ca10
AlexandreGazagnes <a.gazagnes@gmail.com>
2019-04-05 11:49:07 +0200

    commit

---
+++ data/README.md
@@ -0,0 +1,42 @@
+
+PAYS
+-----------------
+
+1 US / UK / France / Espagne / Allemagne / Italie / Chine / Japon
+2 Russie / Bresil / Inde / Belgique / Canada / 
+
+
+TIME 
+-----------------
+
+1 2015 - ojd
+2 2007 - ojd
+3 1990 - ojd
+4 1900 - ojd 
+
+
+DATA
+-----------------
+
+1 Immobilier : 
+- prix immobilier vacant/occupé
+- prix loyers
+- montant / nombre des transactions immo / niveau des ventes
+- niveau moyen des transactions
+
+2 Macro : 
+- PIB / montant / taux
+- niveau d'endettement des menages / entreprises / etats
+- inflation globale / prix alimentaires
+- revenu min / moyens / median de la pouplation 
+
+3 Finance : 
+- niveau des dividendes
+- indice boursiers (cac Dow nasdaq, FTSE, dax, nikkei)
+- resultats des entreprises
+- taux d'intérets 3 / 10 ans
+- bond /obligation d'etat
+
+4 Autres : 
+- prix du baril
+- prix or
--- main.py
+++ main.py
@@ -10,77 +10,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+# plot
 %matplotlib
 sns.set()
-# # filepath
-# os.getcwd()
-# s = [i for i in os.listdir("data/Eur/raw/") if "minimum" in i][0]
-# # data
-# data = pd.read_csv(f'data/Eur/raw/{s}', sep="\t", index_col=0)
-# # first transfo
-# data = data.T
-# data.index = [  i.replace("S1", "-06-30").replace("S2", "-12-31").strip()
-#                 for  i in data.index]
-# data.index = [pd.Timestamp(i) for i in data.index]
-# data = data.iloc[:-1, :]
-# # sanity checks
-# data.head()
-# data.dtypes.head()
-# data.iloc[0, 0]
-# # type casting
-# for col in data.columns :
-#     try :
-#         data[col] = data[col].astype(np.float32)
-#     except TypeError :
-#         data[col] = np.nan
-#     except ValueError :
-#         data[col] = np.nan
-# # sanity checks
-# data.dtypes.head()
-# data.head()
-# # got to csv
-# data.to_csv(f"data/Eur/df/{s}")
-#
 
-# filepath
-s = [i for i in os.listdir("data/Eur/raw/") if "empty" in i][0]
-s
+
+# min revenue
+#####################
+
 # data
-data = pd.read_csv(f'data/Eur/raw/{s}', sep="\t", index_col=0)
+s = [i for i in os.listdir("data/Eur/raw/") if "minimum" in i][0]
+df = pd.read_csv(f'data/Eur/raw/{s}', sep="\t", index_col=0)
 # first transfo
-data = data.T
-data.index = [  i.replace("Q1", "-03-31").replace("Q2", "-06-30")\
+df = df.T
+df.index = [  i.replace("S1", "-06-30").replace("S2", "-12-31").strip()
+                for  i in df.index]
+df.index = [pd.Timestamp(i) for i in df.index]
+df = df.iloc[:-1, :]
+for col in df.columns : # type casting
+    try : df[col] = df[col].astype(np.float32)
+    except TypeError : df[col] = np.nan
+    except ValueError : df[col] = np.nan
+# df.to_csv(f"data/Eur/df/{s}") # go to csv
+# just selec Fr revenues
+cols = [i for i in df.columns if ("eur" in i.lower() and "fr" in i.lower())]
+min_revenue = df.loc[:, cols]
+
+
+# house prices
+#####################
+
+# data
+s = [i for i in os.listdir("data/Eur/raw/") if "empty" in i][0]
+df = pd.read_csv(f'data/Eur/raw/{s}', sep="\t", index_col=0)
+# first transfo
+df = df.T
+df.index = [  i.replace("Q1", "-03-31").replace("Q2", "-06-30")\
                 .replace("Q3", "-09-30").replace("Q4", "-12-31").strip()
-                for i in data.index]
-data.index = [pd.Timestamp(i) for i in data.index]
-data = data.iloc[:-1, :]
-# sanity checks
+                for i in df.index]
+df.index = [pd.Timestamp(i) for i in df.index]
+df = df.iloc[:-1, :]
+# select fr only
+cols = [ i for i in  df.columns if "fr" in i.lower()]
+_df = df.loc[:, cols].iloc[:-8, :]
+# cast
+_df = _df.apply(lambda i : i.apply(lambda j : j.replace("p", "").strip()), axis=1)
+for col in _df.columns :
+    try : _df[col] = _df[col].astype(np.float32)
+    except : print(col)
+# select indice not % of var
+cols = [i for i in _df.columns if (_df[i].mean() > 50)]
+cols = [i for i in cols if (("total" in i.lower()) and ("fr" in i.lower()))]
+house_prices = _df.loc[:, cols]
+house_prices
+
+
+# mix
+#####################
+
+min_revenue["date"] = min_revenue.index
+house_prices["date"] = house_prices.index
+data = pd.merge(house_prices, min_revenue, on="date")
+data.head(7)
+data.set_index("date", drop=True, inplace=True)
+data.columns = ["house_prices", "min_revenue"]
+data["house_prices"] = data["house_prices"] * 100 / data["house_prices"].iloc[5]
+data["min_revenue"] = data["min_revenue"] * 100 / data["min_revenue"].iloc[5]
 data.head()
-data.dtypes.head()
-data.iloc[0, 0]
-
-
-cols = [ i for i in  data.columns if "fr" in i.lower()]
-_data = data.loc[:, cols]
-_data = _data.iloc[:-6, :]
-
-for col in _data.columns :
-    _data[col] = _data[col].apply(lambda i : i.replace("p", "").strip())
-    try :
-        _data[col] = _data[col].astype(np.float32)
-    except :
-        print(col)
-
-
-_data
-
-cols = [i for i in _data.columns if (_data[i].mean() > 50)]
-
-
-#
-# col = _data.columns[0]
-# col
-#
-# _data[col].mean()
-
-_data.plot()
+# data["indice"] = data.iloc[:, 0] /data.iloc[:, 1]
+data.plot()
